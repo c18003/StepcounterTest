2d250b6f8b2d5d6d319ef3981a33f193550fcaa4
c18003 <ohs3126@gmail.com>
2020-01-21 11:23:54 +0900

    てきとう2

--- app/src/main/java/com/example/stepcountertest/MainActivity.java
+++ app/src/main/java/com/example/stepcountertest/MainActivity.java
@@ -7,6 +7,10 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.os.Bundle;
 
@@ -23,51 +27,65 @@ public class MainActivity extends Activity implements SensorEventListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        manager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        manager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
         stepCntSensor = manager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
 
-        textView2 = (TextView)findViewById(R.id.textView2);
+        textView2 = (TextView) findViewById(R.id.textView2);
 
         textView2.setText(" ");
     }
 
 
-
-
-
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy){
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
 
     @Override
-    public void onSensorChanged(SensorEvent event){
+    public void onSensorChanged(SensorEvent event) {
 
         Sensor sensor = event.sensor;
         float[] values = event.values;
         long timestamp = event.timestamp;
 
 
-        if(sensor.getType() == sensor.TYPE_STEP_COUNTER){
-            Log.d("type_step_counter",String.valueOf(values[0]));
+        if (sensor.getType() == sensor.TYPE_STEP_COUNTER) {
+            Log.d("type_step_counter", String.valueOf(values[0]));
             stepcount2++;
             textView2.setText(" " + stepcount2 + " /歩");
         }
     }
 
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
 
-        manager.registerListener(this,stepCntSensor,SensorManager.SENSOR_DELAY_NORMAL);
+        manager.registerListener(this, stepCntSensor, SensorManager.SENSOR_DELAY_NORMAL);
 
     }
 
     @Override
-    protected void onPause(){
+    protected void onPause() {
         super.onPause();
 
-        manager.unregisterListener(this,stepCntSensor);
+        manager.unregisterListener(this, stepCntSensor);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mokuhyou:
+                // ボタンをタップした際の処理を記述
+                break;
+        }
+        return true;
     }
 }
